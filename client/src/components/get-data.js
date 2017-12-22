1930aeff88bbef5e901d6dee9c92be961090f0c7
Francis Redido <sicnarfodider@hotmail.com>
2017-12-22 14:11:33 -0800

    api feature complete

--- client/src/components/get-data.js
+++ client/src/components/get-data.js
@@ -1,7 +1,23 @@
 import React from 'react';
-
+import axios from 'axios';
 
 export default class GetData extends React.Component{
+
+
+  componentDidMount(){
+    this.getData();
+    this.getHumans();
+  }
+  async getData(){
+    const URL = "/api/get-stuff";
+    const response = await axios.get(URL);
+    console.log('Resp', response);
+  }
+  async getHumans(){
+    const URL = "/api/get-humans";
+    const response = await axios.get(URL);
+    console.log('Resp', response);
+  }
   render(){
     return(
       <div>
